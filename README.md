b66abf6c54b4211608c28ed4df03b80b41a335c1
Denise Baker <denviol@yahoo.com>
2019-09-15 13:16:29 -0500

    final version

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# README.md
+# Personal Bio Site
--- bio.js
+++ bio.js
@@ -3,13 +3,25 @@ console.log('bio page');
 const createBio = () => {
     let bioString = '';
     bioString += `
-    <p>bio info here. blah blah blah blah blah blah blah blah blah blah blah  
-    blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-    blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-    blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-    blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-    blah blah blah blah blah blah blah blah blah blah blah blah blah blah 
-    blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>   
+    <p>
+    <li>Professional Violinist for the last 25 years</li>
+    <li>Member of Nashville Symphony for 23 years, permanent 4th chair</li>
+    <li>Winner of 14 Grammy Awards as member of Nashville Symphony</li>
+    <li>Recordings with professional artists including Garth Brooks, Matchbox 20, Taylor Swift</li>
+    <li>Music video with Tim McGraw</li>
+    <li>Movie soundtrack for Bridget Jones Diary</li>
+    <li>4 Xmas tours with Amy Grant</li>
+    <li>Extensive performances throughout Europe</li>
+    <li>Performances at Carnegie Hall</li>
+    <li>Solo Concerto performances with the Nashville Philharmonic, Nashville Concerto Orchestra, Western
+        Piedmont Symphony
+    </li>
+    <li>Solo performances include Bachinalia Festival, Merkin Hall in NYC, Blair School of Music, Renaissance
+        Center
+    </li>
+    <li>Adjunct Professor of Violin at Blair School of Music for 14 years.
+    
+    </p>   
     `
     printToDom(bioString, 'bioPage')
     printToDom('', 'projectsPage')
